498daf6526ce3c21e0e92907c10d781de382a1a6
BootSectorAlpha <lutiell@hotmail.com>
2022-06-22 19:43:15 -0300

    adição em Startup de compatib. MVC, CategoriaRespositorio e CategoriaServico

--- CitelP/CitelP/Startup.cs
+++ CitelP/CitelP/Startup.cs
@@ -1,3 +1,4 @@
+using CitelP.Servicos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -15,59 +16,65 @@ using System.Threading.Tasks;
 
 namespace CitelP
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CitelP", Version = "v1" });
-            });
-
-            string mySQLConnection = Configuration.GetConnectionString("DefaultConnection");
-            services.AddDbContext<AppDbContext>(options =>
-            options.UseMySql(mySQLConnection, ServerVersion.AutoDetect(mySQLConnection)));
-
-            services.AddCors(options =>
-            {
-              options.AddPolicy("CorsPolicy",
-                builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
-                );
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CitelP v1"));
-            }
-
-            app.UseCors("CorsPolicy");
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+      services.AddControllers();
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "CitelP", Version = "v1" });
+      });
+
+      string mySQLConnection = Configuration.GetConnectionString("DefaultConnection");
+      services.AddDbContext<AppDbContext>(options =>
+      options.UseMySql(mySQLConnection, ServerVersion.AutoDetect(mySQLConnection)));
+
+      services.AddCors(options =>
+      {
+        options.AddPolicy("CorsPolicy",
+          builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+          );
+      });
+
+      services.AddScoped<ICategoriaRepositorio, CategoriaRepositorio>();
+      services.AddScoped<ICategoriaServico, CategoriaServico>();
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CitelP v1"));
+      }
+
+      app.UseCors("CorsPolicy");
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
