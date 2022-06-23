dcce19edacf5db42abd034b0cf91010dafa685f4
BootSectorAlpha <lutiell@hotmail.com>
2022-06-23 18:39:58 -0300

    imp. metodo post

---
+++ CitelP/CitelP/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,19 @@
+using AutoMapper;
+using CitelP.Models;
+using CitelP.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CitelP.Mapping
+{
+  public class ResourceToModelProfile : Profile
+  {
+      public ResourceToModelProfile()
+      {
+        CreateMap<SaveCategoriaResource, Categoria>();
+      }
+    
+  }
+}
