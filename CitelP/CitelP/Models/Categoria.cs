edcf1cc6d23339da02ec42acc3f54e6d79831587
BootSectorAlpha <lutiell@hotmail.com>
2022-06-24 21:18:36 -0300

    Exclusão de Required da model Categoria

--- CitelP/CitelP/Models/Categoria.cs
+++ CitelP/CitelP/Models/Categoria.cs
@@ -11,7 +11,7 @@ namespace CitelP.Models
     [Required]
     public int Id { get; set; }
 
-    [Required]
+  
     public string Nome { get; set; }
     public IList<Produto> Produto { get; set; } = new List<Produto>();
   }
