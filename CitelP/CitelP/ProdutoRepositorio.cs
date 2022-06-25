5248499d40c5ef98e2b2ec1499ae42c16e814c35
BootSectorAlpha <lutiell@hotmail.com>
2022-06-25 11:44:06 -0300

    imp. da IProdutoRepositorio na ProdutoRepositorio

--- CitelP/CitelP/ProdutoRepositorio.cs
+++ CitelP/CitelP/ProdutoRepositorio.cs
@@ -19,5 +19,11 @@ namespace CitelP
       return await _context.Produtos.Include(p => p.Categoria)
                                     .ToListAsync();
     }
+
+    public async Task AddAsync(Produto produto)
+    {
+      await _context.Produtos.AddAsync(produto);
+    }
+
   }
 }
